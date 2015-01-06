3f3aa83e2dca29f3a0db3c5b6c1e774f558f25a7
Lori Zanotta <lori.zanotta@gmail.com>
2015-01-06 14:25:56 -0800

    bottles of beer

---
+++ variables.js
@@ -0,0 +1,6 @@
+var bottlesOfBeer = 99;
+for (var i= 0; i < 99; i++) {
+    console.log(bottlesOfBeer + " bottles of beer on the wall, " + bottlesOfBeer + " bottles of beer, take one down, pass it around, " + (bottlesOfBeer - 1) + "bottles of beer on the wall!");
+    bottlesOfBeer = bottlesOfBeer - 1;
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer, take one down, pass it around, no more bottles of beer on the wall!");
